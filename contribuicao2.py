d9f085bc5d6ef3a4d4ac4c7c1b1a9418d8a6701d
Ulric Marques <ulricmf@gmail.com>
2018-08-06 12:31:51 -0300

    Adicionando função pode_mover()

--- contribuicao2.py
+++ contribuicao2.py
@@ -7,6 +7,20 @@ class Seta():
 	def mostra_seta(self):
 		print(self.sentido, end=' ')
 
+	def pode_mover(self):
+		if self.sentido == 'vazio':
+			return False
+
+
+		if self.sentido == '->':
+			if (self.proximo.sentido == 'vazio' or (self.proximo.sentido != 'vazio' and self.proximo.sentido != self.sentido)):
+				return True
+		else:
+			if (self.anterior.sentido == 'vazio' or (self.anterior.sentido != 'vazio' and self.anterior.sentido != self.sentido)):
+				return True
+
+		return False
+
 
 class Estado():
 	def __init__(self, quant_setas):
@@ -21,15 +35,15 @@ class Estado():
 			self.mapa.append(setaAtual)
 			i+=1
 
-		setaAtual = Seta('vazio', None, setaAtual)
+		setaAtual.proximo = Seta('vazio', None, setaAtual)
+		setaAtual = setaAtual.proximo
 		self.mapa.append(setaAtual)
 		i = 0
 		while i < quant_setas:
-			setaAtual.proximo = Seta('<-', setaAtual, None)
+			setaAtual.proximo = Seta('<-', None, setaAtual)
 			setaAtual = setaAtual.proximo
 			self.mapa.append(setaAtual)
 			i+=1
-
 
 
 	def mostra_mapa(self):
@@ -38,9 +52,10 @@ class Estado():
 		print('\n#########')
 
 
-
 estado = Estado(4)
 estado.mostra_mapa()
+for s in estado.mapa:
+	print(s.sentido, s.pode_mover())
 
 
 
